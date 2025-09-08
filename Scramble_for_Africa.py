a8135303107fde66da2db4163c5a477a2c707526
Viktor-Butkovich <51521287+Viktor-Butkovich@users.noreply.github.com>
2025-09-08 09:12:04 -0500

    Added arriving steamships tooltip indicator

--- Scramble_for_Africa.py
+++ Scramble_for_Africa.py
@@ -51,10 +51,8 @@ except Exception:  # displays error message and records error message in crash l
 # Fix issue where some objects persist after exiting to main menu and re-loading (but not after restarting the whole program) - clearly a remove() issue
 #   Implement a "trap" to detect any leaked tiles, cells, grids, or mobs after exit to main menu
 #       Upon detection, immediately log as much as possible and trigger a crash for future diagnosis
-# Incorporate new Asia grid song and 6 other songs
 # Upon minister appointment, have a greyed out version of their candidate portrait, allowing player to reverse or scroll either way
 # Always show an inventory tab for units that can ever pick up anything, and show a button to equip a piece of equipment present in the tile?
-# Re-record Asian grid song
 # Sort inventory display in ascending order, such that most abundant items are shown last and do not block other items
 # Possible issue with overlapping sounds muffling in late-game, with lots of units
 # Add notification whenever worker prices change - at start of turn or after action/hire
@@ -63,13 +61,8 @@ except Exception:  # displays error message and records error message in crash l
 #       Customize start of message based on what caused the price change
 # Add equip all button from top perspective - equip all from tile or from mob
 # Give notification when ship fails to assign an end of turn destination
-# Prompt to name a settlement when it first appears, using a default as the randomly generated name - also extend to SE
 # Add label icon to slave traders and Europe abstract tiles - allows easily knowing what it is without clicking
-# Number of workers not being maintained correctly
 # Attacking ferry incorrectly causing disorganized (if from connected tile)
-# Add steamship destination tooltip to Asia grid
-#   Any tile should have a tooltip when it is a destination for any unit, and for any units it is a destination for
-# Add notification for failed attempt to click on end turn destination
 # Buy consumer goods tooltip should explain how they are used to trade with villages
 # Add achievement for building railroad bridge
 # Add warehouses to trading posts
