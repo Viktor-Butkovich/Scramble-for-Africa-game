3647b79df5dfae226a5b627ef283f0d11161dbc5
Viktor-Butkovich <51521287+Viktor-Butkovich@users.noreply.github.com>
2025-10-04 18:48:27 -0500

    Fixed combat sound application

--- modules/action_types/suppress_slave_trade.py
+++ modules/action_types/suppress_slave_trade.py
@@ -70,7 +70,13 @@ class suppress_slave_trade(action.action):
         """
         audio = super().generate_audio(subject)
         if subject == "roll_started":
+            if self.current_unit.equipment.get("Maxim gun", False):
+                audio.append("effects/maxim_gun")
             audio.append("effects/gunfire")
+            if random.randrange(1, 7) >= 4:
+                audio.append("effects/battle_1")
+            else:
+                audio.append("effects/battle_2")
         return audio
 
     def generate_current_roll_modifier(self):
